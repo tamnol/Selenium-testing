d85e2520e3a6b9c5574d93a8bd9eda3dd9c7245e
Lucien Tamno <armandino2009@gmail.com>
2021-10-05 19:53:34 -0700

    Update Form.java

--- Ex_Files_Selenium_EssT/Ex_Files_Selenium_EssT/Exercise Files/06_04/end/test/Form.java
+++ Ex_Files_Selenium_EssT/Ex_Files_Selenium_EssT/Exercise Files/06_04/end/test/Form.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class Form {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver","C:/Users/arman/Downloads/Selenium/chromedriver_win32/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
